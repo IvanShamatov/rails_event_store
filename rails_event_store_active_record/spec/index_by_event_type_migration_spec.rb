c1be089290b48dc410a00976627c7bf06a2aa811
Paweł Pacana <pawel.pacana@gmail.com>
2018-11-16 00:01:12 +0100

    Run all migrations from last released version up to current schema.

--- rails_event_store_active_record/spec/index_by_event_type_migration_spec.rb
+++ rails_event_store_active_record/spec/index_by_event_type_migration_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe "index_by_event_type_migration" do
           .run_migration('create_event_store_events', 'migration')
       EOF
       run_migration('index_by_event_type')
+      run_migration('binary_data_and_metadata')
       expect(dump_schema).to eq(current_schema)
     ensure
       drop_database
